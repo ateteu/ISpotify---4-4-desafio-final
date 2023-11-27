736a8bcf071ab0de4f07edf727dde8a0a2036fc2
Mateus <mr.lordblader@gmail.com>
2023-11-27 10:44:52 -0300

    trocar img estatica artistas por api map. nao esta funcionando

--- client/src/Body.jsx
+++ client/src/Body.jsx
@@ -1,60 +1,57 @@
 import './main.css'
 import BlocoArtista from './BlocoArtista.jsx';
 
+// Imports do React:
+import React, { useState, useEffect } from "react"
+
 function Body() {
+
+    const [error, setError] = useState("");
+
+    // Função para fazer pegar os dados dos artistas:
+
+    const [artistas, setArtistas] = useState([]);
+    useEffect(() => {
+        async function getArtists() {
+            try {
+                const resposta = await api.get("/artists");
+                setArtistas(resposta.data);
+            
+                console.log("Sucesso! Dados dos artistas obtidos.");
+            }
+            catch(error) {
+                setError(error.resposta.data);
+                console.log("Houve um erro na requisição de dados dos artistas!");
+                console.log(error);
+            }
+        }
+        
+        getArtists();
+
+    }, []); // O array vazio [] garante a execução única do getArtists
     
     return (
     <div id="body">
 
         <div id="tituloBody"> Artistas </div>
-
         <div id="linhasBody">
-            <div id="linha1Body">
-                <BlocoArtista 
-                    srcFotoArtista="./images/engenheiros.png"
-                    nomeArtista="Engenheiros do Hawaii"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/cidadeNegra.png"
-                    nomeArtista="Cidade Negra"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/capital.png"
-                    nomeArtista="Capital Inicial"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/skank.png"
-                    nomeArtista="Skank"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/paralamas.png"
-                    nomeArtista="Paralamas do Sucesso"
-                />
-            </div>
-
-            <div id="linha2Body">
-                <BlocoArtista 
-                    srcFotoArtista="./images/lulu.png"
-                    nomeArtista="Lulu Santos"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/cazuza.png"
-                    nomeArtista="Cazuza"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/kidAbelha.png"
-                    nomeArtista="Kid Abelha"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/biquini.png"
-                    nomeArtista="Biquini Cavadão"
-                />
-                <BlocoArtista 
-                    srcFotoArtista="./images/rappa.png"
-                    nomeArtista="O Rappa"
-                />
-            </div>
+            {artistas.map((item, i) => {return (
+                <div key={i}>
+                    <img
+                        src={ item.image }
+                        width={ 100 }
+                        style={{ borderRadius: "50%" }}
+                    />
+                </div>
+                ); })
+            }
         </div>
+
+        {error 
+            // Em caso de erro, retorna uma mensagem de erro
+            ? <Alert severity="error"> { error } </Alert> 
+            : <div />
+        }
 
     </div>
     );
